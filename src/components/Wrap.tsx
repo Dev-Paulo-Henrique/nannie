cb91404fa1e68ee73d6faa44052009cfb059aa15
Paulo Henrique <jwcfree11@gmail.com>
2023-12-25 14:08:23 -0300

    first commit

--- src/components/Wrap.tsx
+++ src/components/Wrap.tsx
@@ -9,7 +9,7 @@ export function Item({text}: WrapItemProps) {
   return (
     <WrapItem alignItems="center">
       <CircleListIcon />
-      <Text color="gray.600" ml={2} fontSize={{base: 14, md: 18}} w={{base: "150px", md: "180px"}}>{text}</Text>
+      <Text color="gray.600" ml={2} fontSize={{base: 14, md: 18}} w={{base: "130px", md: "180px"}}>{text}</Text>
     </WrapItem>
   );
 }
